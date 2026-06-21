09eb82b34b34181b9645e1d181a554d960dff11e
Yasin <yasinpeker43@gmail.com>
2026-06-21 16:33:48 +0300

    initialized the frontend repository for shortener API

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+    <h1 class="text-4xl font-bold text-emerald-400">
+      Tailwind v4 Hazır!
+    </h1>
+  </div>
+</template>
