0f3f27cb69d20f933c1b625ae34410884ccc9b01
Yasin <yasinpeker43@gmail.com>
2026-06-22 08:56:09 +0300

    add dependencies to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { OpenAPI } from './services/api'
+
+OpenAPI.BASE=import.meta.env.VITE_API_BASE_URL;
 
 createApp(App).mount('#app')
