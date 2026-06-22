cc91fdc592a804ee46a2e3631cb4b3c032e659b2
Yasin <yasinpeker43@gmail.com>
2026-06-22 09:05:45 +0300

    add router to the project

---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-8">
+       <h2 class=" text-2xl font-bold text-shadow-cyan-400">Homepage</h2>
+    </div>
+</template>
---
+++ src/views/Register.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-8">
+       <h2 class=" text-2xl font-bold text-shadow-cyan-400">Register</h2>
+    </div>
+</template>
