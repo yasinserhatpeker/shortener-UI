b8558199607b99eb63ec28ee5cfa52704c0ddb1a
Yasin <yasinpeker43@gmail.com>
2026-06-22 09:11:49 +0300

    router logic is done

---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+import { createRouter,createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+
+const routes = [
+    {path: '/', name:'Home', component:Home},
+    {path:'/register', name:'Register', component: () => import('../views/Register.vue')},
+    {path:'/login', name:'Login', component: () => import('../views/Login.vue')}
+]
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+})
+
+export default router
